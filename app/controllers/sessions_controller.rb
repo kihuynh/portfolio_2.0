e49c6da174ba4ba608b82f5e8e2731d408c2e316
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-27 11:56:26 -0700

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.authenticate(params[:name], params[:email], params[:password])
+    if @user
+      flash[:notice] = "You've signed in."
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem signing in. Please try again."
+      redirect_to signin_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've signed out."
+    redirect_to "/"
+  end
+end
