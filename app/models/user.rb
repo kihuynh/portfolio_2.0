9ce2220e7585597aa647fbdeac1b06f3c5fad186
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-27 13:21:15 -0700

    made singular and fixed uninitialized constant problem

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  attr_accessor :password
+  validates_confirmation_of :password
+  validates :email, :presence => true, :uniqueness => true
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by "email = ?", email
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+end
