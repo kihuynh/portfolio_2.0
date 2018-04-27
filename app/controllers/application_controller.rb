b287ee4fcbef48cdfc416d03420547c6ed2e0cd0
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-27 11:58:14 -0700

    add the double pipes for current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user = User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
   end
 end
