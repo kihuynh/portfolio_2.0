57267639598563ccbe2d27cd718699d5ca6c4e68
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-27 11:54:44 -0700

    add name permit to user params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Thank you for signing up!"
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
